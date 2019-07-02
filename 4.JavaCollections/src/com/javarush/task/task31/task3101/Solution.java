6668a504dfd820b138f884056645ced88bd69f24
mpermyak <mpermyak@trilpay.com>
2019-07-03 00:09:13 +0700

    in progress

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,40 +1,49 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /*
 Проход по дереву файлов
 */
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-        File folder = new File(args[0]);
-        File resultFileAbsolutePath = new File(args[1]);
-        if (FileUtils.isExist(resultFileAbsolutePath)) {
-            FileUtils.renameFile(resultFileAbsolutePath, new File(resultFileAbsolutePath.getParent() + "allFilesContent.txt"));
-        }
-
-        ArrayList<String> sortedFileList = new ArrayList<>();
-        Map<String, String> fNames = new HashMap<>();
-//        folder.isFile();//todo
-        for (File file : folder.listFiles()) {
-            if (file.length() <= 50) {
-                fNames.put(file.getAbsolutePath(), file.getName());
-            }
-        }
-
-        sortedFileList = new ArrayList<>(fNames.values());
-        sortedFileList.sort(String::compareTo);
-
-        InputStream inputStream;
-        OutputStream outputStream = new FileOutputStream(resultFileAbsolutePath);
-
-        for (String fName : sortedFileList) {
-            inputStream = new FileInputStream(fName);
-            while (true) {
-            }
-        }
+  public static void main(String[] args) throws IOException {
+    File resultFileAbsolutePath = new File(args[1]);
+    if (FileUtils.isExist(resultFileAbsolutePath)) {
+      FileUtils.renameFile(resultFileAbsolutePath, new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt"));
     }
+
+    List<File> files;
+    files = getFileListingNoSort(new File(args[0]));
+    Collections.sort(files);
+
+    OutputStream outputStream = new FileOutputStream(resultFileAbsolutePath);
+    InputStream inputStream;
+    for (File file : files) {
+      inputStream = new FileInputStream(file);
+      while (inputStream.available() > 0) {
+        outputStream.write(inputStream.read());
+      }
+      outputStream.write(10);
+    }
+
+    outputStream.close();
+  }
+
+  private static List<File> getFileListingNoSort(File startingDir) {
+    List<File> result = new ArrayList<>();
+    File[] filesAndDirs = startingDir.listFiles();
+    List<File> filesDirs = Arrays.asList(filesAndDirs);
+    for (File file : filesDirs) {
+      if (file.length() < 50)
+        result.add(file); //always add, even if directory
+      if (!file.isFile()) {
+        //must be a directory
+        //recursive call!
+        List<File> deeperList = getFileListingNoSort(file);
+        result.addAll(deeperList);
+      }
+    }
+    return result;
+  }
 }
