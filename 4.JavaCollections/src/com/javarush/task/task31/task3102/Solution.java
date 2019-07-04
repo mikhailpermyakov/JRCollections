54570bcf90a6ff9af253d508413737192e9991bd
mpermyak <mpermyak@trilpay.com>
2019-07-04 23:50:46 +0700

    in progress

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -8,13 +8,45 @@ import java.util.*;
 Находим все файлы
 */
 public class Solution {
-    public static List<String> getFileTree(String root) {
-        return new ArrayList<>(Arrays.asList(Objects.requireNonNull(new File(root).list())));
+  private static List<String> getFileTree(String root) {
+    List<String> result = new ArrayList<>();
+    Queue<File> dirToWalk = new PriorityQueue<>();
 
+    File[] initialFileList = new File(root).listFiles();
+
+    if (null != initialFileList && initialFileList.length > 0) {
+      for (File file : initialFileList) {
+        if (file.isDirectory())
+          dirToWalk.add(file);
+
+        if (file.isFile())
+          result.add(file.getAbsolutePath());
+      }
     }
 
-    public static void main(String[] args) {
-        List<String> fList = getFileTree("D:/d");
-        fList.forEach(System.out::println);
+    while (!dirToWalk.isEmpty()){
+      File[] currentFList = dirToWalk.remove().listFiles();
+
+      if (null != currentFList && currentFList.length > 0) {
+        for (File file : currentFList){
+          if (file.isDirectory()){
+            dirToWalk.add(file);
+          }
+
+          if (file.isFile()) {
+            result.add(file.getAbsolutePath());
+          }
+        }
+      }
     }
+
+
+    return result; //new ArrayList<>(Arrays.asList(Objects.requireNonNull(new File(root).list())));
+
+  }
+
+  public static void main(String[] args) {
+    List<String> fList = getFileTree("D:/d");
+    fList.forEach(System.out::println);
+  }
 }
